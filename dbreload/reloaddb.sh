c24f5eab478737fa231bc1d2c5d671dc8272c4b8
parinisantiago <parinisantiago@gmail.com>
2016-12-15 13:56:37 -0300

    agrego variables de sql

--- dbreload/reloaddb.sh
+++ dbreload/reloaddb.sh
@@ -1,20 +1,24 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PHP=/usr/bin/php5.6;
+usuario="root";
+clave="root";
+dbname="ojs30";
+
 cd $DIR
 echo "borramos la base vieja"
 mysqladmin -f drop ojs30 -uroot -proot
 echo "va volvemos a crear"
 mysqladmin create ojs30 -uroot -proot
 echo "le encajamos la base 2.4"
-mysql -uroot -proot ojs30 < ./db.sql
+mysql -u$usuario -p$clave $dbname < ./db.sql
 echo "le metemos consultas para que todo ande a al hora de migrar"
-mysql -uroot -proot ojs30 < ./extraqueries.sql
+mysql -u$usuario -p$clave $dbname < ./extraqueries.sql
 echo "pasamos las tablas a utf8"
 python ./collation.py
 echo "empieza la migracion!"
 $PHP ../tools/upgrade.php upgrade
 echo "Vamow a ejecutar las queries faltantes"
-mysql -uroot -proot ojs30 < ./postqueries.sql
+mysql -u$usuario -p$clave $dbname < ./postqueries.sql
 echo "ahora vamos con un par de cositas mas de python"
 python ./pdf.py
 echo "terminamos, gapish"
