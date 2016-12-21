34bd0dfffbd8dab3da69c62390088bf07c12a3bc
Parini Santiago <parinisantiago@gmail.com>
2016-12-21 15:21:45 -0300

    permisos para uploads y arreglo para el buscador public

--- dbreload/reloaddb.sh
+++ dbreload/reloaddb.sh
@@ -5,6 +5,8 @@ clave="root";
 dbname="ojs30";
 host="localhost";
 cd $DIR
+echo"Cambiando permisos de carpetas"
+chmod 755 -R ../uploads
 echo "Borrando la base"
 mysqladmin -f drop $dbname -u$usuario -p$clave -h$host
 echo "Creando la base"
@@ -21,4 +23,9 @@ echo "Ejecutando postqueries.sql"
 mysql -u$usuario -p$clave -h$host $dbname < ./postqueries.sql
 echo "Ejecutando pdf.py"
 python ./pdf.py $usuario $clave $dbname $host
-echo "Fin de la migración"
+echo "Arreglo para el buscador public"
+$PHP ../tools/rebuildSearchIndex.php
+echo "Regresando permisos a su estado orginal"
+chmod 700 -R ../uploads
+echo "Fin de la migracion"
+
