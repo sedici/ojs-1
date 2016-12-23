5f6c897e0168bf85eab27ed083740870c7eeee97
Parini Santiago <parinisantiago@gmail.com>
2016-12-23 09:59:52 -0300

    saco permisos

--- dbreload/reloaddb.sh
+++ dbreload/reloaddb.sh
@@ -5,11 +5,6 @@ clave="root";
 dbname="ojs30";
 host="localhost";
 cd $DIR
-echo "Cambiando permisos de carpetas"
-sudo chmod 777 -R ../uploads
-sudo chmod 777 -R ../cache
-sudo chmod 777 -R ../public
-sudo chmod 766 ../config.inc.php
 echo "Borrando la base"
 mysqladmin -f drop $dbname -u$usuario -p$clave -h$host
 echo "Creando la base"
@@ -28,7 +23,5 @@ echo "Ejecutando pdf.py"
 python ./pdf.py $usuario $clave $dbname $host
 echo "Arreglo para el buscador public"
 $PHP ../tools/rebuildSearchIndex.php
-echo "Regresando permisos a su estado orginal"
-sudo chmod 700 -R ../uploads
 echo "Fin de la migracion"
 
